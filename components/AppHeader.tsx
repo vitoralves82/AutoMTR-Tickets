9eaeedaebc72795c13819e6a9a7dec307ba264a7
vitoralves82 <vitoralves82@yahoo.com.br>
2025-06-24 22:09:47 -0300

    Update AppHeader.tsx
    nome

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -5,7 +5,7 @@ const AppHeader: React.FC = () => {
   return (
     <header className="w-full text-center py-6">
       <h1 className="text-6xl font-bold text-[rgba(0,90,149,255)] tracking-tight">
-        AutoMMR
+        AutoMMR v2
       </h1>
       <p className="text-xl text-[rgba(0,90,149,255)] mt-2">
         Extraia informações de formulários MMR com facilidade usando IA.
