3ecec8d36629630af61c481c7b3feece1e1d93da
vitoralves82 <vitoralves82@yahoo.com.br>
2025-06-30 14:50:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  // carrega variáveis do .env.[mode]
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    define: {
+      // expõe a variável para o código do browser
+      'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.')
       }
-    };
+    }
+  };
 });
