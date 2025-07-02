0735e123bfbfd30bd180dcc4150030d7dd7ed857
vitoralves82 <vitoralves82@yahoo.com.br>
2025-07-02 20:30:50 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -374,7 +374,7 @@ const App: React.FC = () => {
       <footer className="text-center py-8 text-slate-50 text-sm mt-auto">
         <p>&copy; {new Date().getFullYear()} AutoMMR. Powered by Consultoria ESG - EVP.</p>
         {totalTimeSaved > 0 && (
-            <p className="text-xs text-slate-400 mt-2">
+            <p className="text-base text-slate-200 mt-2">
                 Você já economizou um total de {formatTimeSaved(totalTimeSaved)} da sua vida. Parabéns!
             </p>
         )}
